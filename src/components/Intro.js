617a8ff6c956b48bcd0d4337774326eb90dbd582
Nanouchkaya <brun.claudine@live.fr>
2019-11-16 14:00:03 +0100

    Add intro text and title style

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,12 +5,35 @@ import introStyles from "../assets/styles/intro.module.scss"
 
 const Intro = ({ sal }) => (
   <div
+    className={introStyles.container}
     data-sal={sal[0]}
     data-sal-delay={sal[1]}
     data-sal-easing={sal[2]}
     id="intro"
   >
-    Présentation collectif
+    <h1 className={introStyles.title}>Les créatifs auvergnats</h1>
+    <h2>
+      Par la team <strong>Etsy</strong> auvergne
+    </h2>
+    <p>
+      Le collectif "Les créatifs auvergnats" rassemble des créateurs de notre
+      belle région : l'<strong>auvergne</strong>. Un seul mot d'ordre parmi ces
+      créateurs : le fait-main, et le <strong>fait-main</strong> local ! Sous la
+      bannière "Les créatifs auvergnats" se regroupe plusieurs créateurs qui
+      souhaitent faire connaitre leur savoir-faire et partager avec vous une
+      expérience unique, que ce soit de la maroquinerie, des bijoux en perles,
+      des cartes à planter, des cosmétiques bio ou encore des doudous pour
+      enfants. Un large éventail de créations vous attendent.
+    </p>
+    <p>
+      Plusieurs fois dans l'année, nous organisons des
+      <strong>marchés de créateurs</strong> pour promouvoir le fait-main
+      auvergnat ainsi que des apéros créateurs pour échanger dans nos métiers.
+      Notre souhait pour l'avenir est de mettre en place des
+      <strong>boutiques éphémères</strong> ainsi que de nombreux marchés de
+      créateurs à <strong>Clermont-Ferrand</strong> mais aussi partout en
+      auvergne.
+    </p>
   </div>
 )
 
